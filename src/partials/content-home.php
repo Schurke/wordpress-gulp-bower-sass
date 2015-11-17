3e0851af3b0be15194928cde558cbabdae60b89e
Craig Harwood <schurke@schurke.ca>
2015-11-17 15:27:39 -0500

    revisions from ingenia

--- src/partials/content-home.php
+++ src/partials/content-home.php
@@ -26,50 +26,6 @@
 	</div>
 </section>
 
-
-<section id="feature-blocks">
-	<div class="container">
-		<?php 
-			// output the image title body units that fade in left/right
-			
-			if (get_field('blocks')){
-				while( has_sub_field('blocks') ){ 
-					$style = "";
-					$link = get_sub_field('link');
-					// bg color
-					$background_color = get_sub_field('background_color');
-					$style .= "background-color:".$background_color.'; ';
-					// bg image
-					$background_image = get_sub_field('background_image');
-					// column setup
-					$columns = get_sub_field('columns');
-					if (!$columns) $columns = 1;
-					$col = $columns*4;
-					$read_more_link = "";
-					?>
-
-					<div class="col-sm-<?php echo $col; ?> feature-block" style="<?php echo $style ?>">
-
-
-					<?php
-					// if page is news, get and display latest news item (post)
-
-					// if page is testimonials, get customer_testimonials (field) from page and display text from first in list
-
-					// if page is employee stories, get employee_stories (field) from page and display text from first in list
-
-					// if page is aything else, display title, excerpt
-
-					// output learn more link
-
-					?>
-
-					</div>
-
-					<?php
-
-				}
-			}
-		?>
-	</div>
+<section>
+	
 </section>
